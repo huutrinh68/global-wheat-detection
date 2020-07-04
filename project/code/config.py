7dac870a8a9def655a8bde46a1837ec34f45d79e
huutrinh68 <trinhsp89@gmail.com>
2020-07-04 23:46:11 +0900

    change image to 1024

--- project/code/config.py
+++ project/code/config.py
@@ -8,8 +8,8 @@ class Config():
     device = 'cuda:0'
     seed = 42
     lr = 0.0002
-    n_epochs = 50
-    batch_size = 2
+    n_epochs = 200
+    batch_size = 2 
     num_workers = 4
     step_scheduler = False
     validation_scheduler = True
@@ -17,7 +17,7 @@ class Config():
     scheduler_params = dict(
         mode='min',
         factor=0.5,
-        patience=1,
+        patience=2,
         verbose=False, 
         threshold=0.0001,
         threshold_mode='abs',
--- project/code/main.py
+++ project/code/main.py
@@ -77,9 +77,6 @@ def run_training():
 
 
 
-    
-
-
 if __name__ == '__main__':
     run_training()
 
