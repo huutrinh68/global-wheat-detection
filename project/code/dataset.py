b69478dc4a3f69f617299bcf9c3d6b35e9fb123d
huutrinh68 <trinhsp89@gmail.com>
2020-07-04 23:46:44 +0900

    change image size to 1024

--- project/code/dataset.py
+++ project/code/dataset.py
@@ -15,7 +15,7 @@ def get_train_transforms():
             A.ToGray(p=0.01),
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
-            A.Resize(height=512, width=512, p=1),
+            A.Resize(height=1024, width=1024, p=1),
             A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
             ToTensorV2(p=1.0),
         ],
@@ -32,7 +32,7 @@ def get_train_transforms():
 def get_valid_transforms():
     return A.Compose(
         [
-            A.Resize(height=512, width=512, p=1.0),
+            A.Resize(height=1024, width=1024, p=1.0),
             ToTensorV2(p=1.0),
         ],
         p=1.0,
