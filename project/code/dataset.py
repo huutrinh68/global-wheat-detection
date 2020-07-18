95aefe966a73954651cafcff3595237cfa2d9c29
tonthatnam <thatnamdn@gmail.com>
2020-07-18 18:59:04 +0900

    change augmentation

--- project/code/dataset.py
+++ project/code/dataset.py
@@ -5,20 +5,20 @@ from config import config
 def get_train_transforms():
     return A.Compose(
         [
-            #A.RandomSizedCrop(min_max_height=(800, 800), height=1024, width=1024, p=0.5),
+            A.RandomSizedCrop(min_max_height=(800, 800), height=1024, width=1024, p=0.5),
             A.OneOf([
                 A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2,
                                      val_shift_limit=0.2, p=0.9),
                 A.RandomBrightnessContrast(brightness_limit=0.2,
                                            contrast_limit=0.2, p=0.9),
             ],p=0.9),
-            #A.ToGray(p=0.01),
-            #A.HorizontalFlip(p=0.5),
-            #A.VerticalFlip(p=0.5),
-            #A.Resize(height=1024, width=1024, p=1),
-            #A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
-            A.Flip(p=0.5),
-            A.Rotate(limit=45, p=0.5),
+            A.ToGray(p=0.01),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Resize(height=1024, width=1024, p=1),
+            A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
+            #A.Flip(p=0.5),
+            A.Rotate(limit=90, p=0.5),
             ToTensorV2(p=1.0),
         ],
         p=1.0,
@@ -60,10 +60,12 @@ class WheatDataset(Dataset):
     def __getitem__(self, idx):
         image_id = self.image_ids[idx]
 
-        if self.test or random.random() > 0.5:
+        if self.test or random.random() > 0.6:
             image, boxes = self.load_image_and_boxes(idx)
-        elif random.random() > 0.25:
+        elif random.random() > 0.4:
             image, boxes = self.load_cutmix_image_and_boxes(idx)
+        elif random.random() > 0.2:
+            image, boxes = self.load_image_and_bboxes_with_cutmix(idx)
         else:
             image, boxes = self.load_mixup_image_and_boxes(idx)
 
@@ -77,12 +79,6 @@ class WheatDataset(Dataset):
 
         if self.transforms:
             for i in range(10):
-
-                # sample = self.transforms(**{
-                #     'image': image,
-                #     'bboxes': target['boxes'],
-                #     'labels': labels
-                # })
 
                 sample = self.transforms(**{
                     'image': image,
@@ -164,6 +160,81 @@ class WheatDataset(Dataset):
         result_boxes = result_boxes[np.where((result_boxes[:,2]-result_boxes[:,0])*(result_boxes[:,3]-result_boxes[:,1]) > 0)]
 
         return result_image, result_boxes
+
+    def load_image_and_bboxes_with_cutmix(self, idx):
+        image, bboxes = self.load_image_and_boxes(idx)
+        image_to_be_mixed, bboxes_to_be_mixed = self.load_image_and_boxes(
+            random.randint(0, self.image_ids.shape[0] - 1))
+
+        image_size = image.shape[0]
+        cutoff_x1, cutoff_y1 = [int(random.uniform(image_size * 0.0, image_size * 0.49)) for _ in range(2)]
+        cutoff_x2, cutoff_y2 = [int(random.uniform(image_size * 0.5, image_size * 1.0)) for _ in range(2)]
+
+        image_cutmix = image.copy()
+        image_cutmix[cutoff_y1:cutoff_y2, cutoff_x1:cutoff_x2] = image_to_be_mixed[cutoff_y1:cutoff_y2,
+                                                                 cutoff_x1:cutoff_x2]
+
+        # Begin preparing bboxes_cutmix.
+        # Case 1. Bounding boxes not intersect with cut off patch.
+        bboxes_not_intersect = bboxes[np.concatenate((np.where(bboxes[:, 0] > cutoff_x2),
+                                                      np.where(bboxes[:, 2] < cutoff_x1),
+                                                      np.where(bboxes[:, 1] > cutoff_y2),
+                                                      np.where(bboxes[:, 3] < cutoff_y1)), axis=None)]
+
+        # Case 2. Bounding boxes intersect with cut off patch.
+        bboxes_intersect = bboxes.copy()
+
+        top_intersect = np.where((bboxes[:, 0] < cutoff_x2) &
+                                 (bboxes[:, 2] > cutoff_x1) &
+                                 (bboxes[:, 1] < cutoff_y2) &
+                                 (bboxes[:, 3] > cutoff_y2))
+        right_intersect = np.where((bboxes[:, 0] < cutoff_x2) &
+                                   (bboxes[:, 2] > cutoff_x2) &
+                                   (bboxes[:, 1] < cutoff_y2) &
+                                   (bboxes[:, 3] > cutoff_y1))
+        bottom_intersect = np.where((bboxes[:, 0] < cutoff_x2) &
+                                    (bboxes[:, 2] > cutoff_x1) &
+                                    (bboxes[:, 1] < cutoff_y1) &
+                                    (bboxes[:, 3] > cutoff_y1))
+        left_intersect = np.where((bboxes[:, 0] < cutoff_x1) &
+                                  (bboxes[:, 2] > cutoff_x1) &
+                                  (bboxes[:, 1] < cutoff_y2) &
+                                  (bboxes[:, 3] > cutoff_y1))
+
+        # Remove redundant indices. e.g. a bbox which intersects in both right and top.
+        right_intersect = np.setdiff1d(right_intersect, top_intersect)
+        right_intersect = np.setdiff1d(right_intersect, bottom_intersect)
+        right_intersect = np.setdiff1d(right_intersect, left_intersect)
+        bottom_intersect = np.setdiff1d(bottom_intersect, top_intersect)
+        bottom_intersect = np.setdiff1d(bottom_intersect, left_intersect)
+        left_intersect = np.setdiff1d(left_intersect, top_intersect)
+
+        bboxes_intersect[:, 1][top_intersect] = cutoff_y2
+        bboxes_intersect[:, 0][right_intersect] = cutoff_x2
+        bboxes_intersect[:, 3][bottom_intersect] = cutoff_y1
+        bboxes_intersect[:, 2][left_intersect] = cutoff_x1
+
+        bboxes_intersect[:, 1][top_intersect] = cutoff_y2
+        bboxes_intersect[:, 0][right_intersect] = cutoff_x2
+        bboxes_intersect[:, 3][bottom_intersect] = cutoff_y1
+        bboxes_intersect[:, 2][left_intersect] = cutoff_x1
+
+        bboxes_intersect = bboxes_intersect[np.concatenate((top_intersect,
+                                                            right_intersect,
+                                                            bottom_intersect,
+                                                            left_intersect), axis=None)]
+
+        # Case 3. Bounding boxes inside cut off patch.
+        bboxes_to_be_mixed[:, [0, 2]] = bboxes_to_be_mixed[:, [0, 2]].clip(min=cutoff_x1, max=cutoff_x2)
+        bboxes_to_be_mixed[:, [1, 3]] = bboxes_to_be_mixed[:, [1, 3]].clip(min=cutoff_y1, max=cutoff_y2)
+
+        # Integrate all those three cases.
+        bboxes_cutmix = np.vstack((bboxes_not_intersect, bboxes_intersect, bboxes_to_be_mixed)).astype(int)
+        bboxes_cutmix = bboxes_cutmix[np.where((bboxes_cutmix[:, 2] - bboxes_cutmix[:, 0]) \
+                                               * (bboxes_cutmix[:, 3] - bboxes_cutmix[:, 1]) > 500)]
+        # End preparing bboxes_cutmix.
+
+        return image_cutmix, bboxes_cutmix
 
     def load_mixup_image_and_boxes(self, idx):
       image, boxes = self.load_image_and_boxes(idx)
