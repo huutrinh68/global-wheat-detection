ecc2ce455688c97652d0f15f56e90e0c3acb1a97
huutrinh68 <trinhsp89@gmail.com>
2020-07-25 22:59:45 +0900

    change logs folder

--- project/code/config.py
+++ project/code/config.py
@@ -3,7 +3,7 @@ from lib import *
 class Config():
     train_csv = './input/global-wheat-detection/train.csv'
     train_imgs = './input/global-wheat-detection/train'
-    checkpoint = './checkpoint-d5-mixup-adam-tune-augs'
+    checkpoint = './checkpoint-d5-master'
     # gpu_ids = [0, 1, 2, 3] # change to gpus you want to use
     gpu_ids = [3]
     if len(gpu_ids) > 1:
